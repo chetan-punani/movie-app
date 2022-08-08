2671362865a1e69e696361f35f2eeb00df06c650
chetan <chetan@gmail.com>
2022-08-08 15:11:34 +0530

    showresult component added

--- src/views/MoreMovies.vue
+++ src/views/MoreMovies.vue
@@ -2,86 +2,28 @@
   <div>
     <div id="resultshow">
       <div v-if="this.gernesList">
-        <div class="row mt-5 mb-5">
-          <div class="ml-10 d-flex justify-content-between">
-            <div>
-              <h3>{{ category }}</h3>
-            </div>
-          </div>
-          <div class="row mt-3">
-            <div class="col">
-              <RouterLink
-                v-for="movie in searchGerneList"
-                :key="movie.id"
-                :to="'/moviedetails/' + movie.id"
-              >
-                <img :src="movie.image" alt="Avatar" id="image" />
-              </RouterLink>
-            </div>
-            <div class="col" v-if="searchGerneList.length === 0">
-              <h5 id="no-data">No data found !!!</h5>
-            </div>
-          </div>
-        </div>
+        <ShowResults call-fun="searchGerneList"></ShowResults>
+      </div>
+       <div v-if="this.searchList">
+        <ShowResults call-fun="searchMovieList"></ShowResults>
+      </div>
+       <div v-if="this.list">
+        <ShowResults call-fun="listMovies"></ShowResults>
       </div>
 
-      <div v-if="this.searchList">
-        <div class="row mt-5 mb-5">
-          <div class="ml-10 d-flex justify-content-between">
-            <div>
-              <h3>{{ category }}</h3>
-            </div>
-          </div>
-          <div class="row mt-3">
-            <div class="col">
-              <RouterLink
-                v-for="movie in searchMovieList"
-                :key="movie.id"
-                :to="'/moviedetails/' + movie.id"
-              >
-                <img :src="movie.image" alt="Avatar" id="image" />
-              </RouterLink>
-            </div>
-            <div class="col" v-if="searchMovieList.length === 0">
-              <h5 id="no-data">No data found !!!</h5>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div v-if="this.list">
-        <div class="row mt-5 mb-5">
-          <div class="ml-10 d-flex justify-content-between">
-            <div>
-              <h3>{{ category }}</h3>
-            </div>
-          </div>
-          <div class="row mt-3">
-            <div class="col">
-              <RouterLink
-                v-for="movie in listMovies"
-                :key="movie.id"
-                :to="'/moviedetails/' + movie.id"
-              >
-                <img :src="movie.image" alt="Avatar" id="image" />
-              </RouterLink>
-            </div>
-            <div class="col" v-if="listMovies.length === 0">
-              <h5 id="no-data">No data found !!!</h5>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from "axios";
+import ShowResults from "@/components/ShowResults.vue";
 
 export default {
   name: "moremovies",
   props: ["category"],
+  components: {
+    ShowResults
+  },
   data: () => {
     return {
       movieList: [],
@@ -90,46 +32,11 @@ export default {
       list: [],
     };
   },
-  computed: {
-    searchGerneList() {
-      return this.movieList.filter((item) => item.genres === this.gernesList);
-    },
-    searchMovieList() {
-      return this.movieList.filter((item) =>
-        item.title.toLowerCase().includes(this.searchList.toLowerCase())
-      );
-    },
-    listMovies() {
-      return this.movieList.slice(8, 16);
-    },
-  },
-  methods: {
-    async movieData() {
-      await axios
-        .get("https://movie-app-26981-default-rtdb.firebaseio.com/data.json")
-        .then((response) => {
-          if (response) {
-            if (response.data) {
-              this.movieList = response.data;
-            }
-          }
-        })
-        .catch((e) => {
-          console.log(e);
-        });
-    },
-    onUpdate() {
-      this.searchGerneList();
-      this.searchMovieList();
-      this.listMovies();
-    },
-  },
   beforeRouteUpdate(to, from, next) {
     next();
     this.$router.go(0);
   },
   mounted() {
-    this.movieData();
     this.gernesList = this.$route.query.gerne;
     this.searchList = this.$route.query.search;
     this.list = this.$route.query.type;
@@ -140,24 +47,5 @@ export default {
 <style scoped>
 #resultshow {
   margin: 20px 30px;
-}
-
-#image {
-  width: 150px;
-  height: 200px;
-  margin: 0 3px;
-  transition: transform 0.2s;
-  box-shadow: 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  border-radius: 10px;
-}
-
-#image:hover {
-  -ms-transform: scale(1.2);
-  -webkit-transform: scale(1.2);
-  transform: scale(1.2);
-}
-
-#no-data {
-  color: brown;
 }
 </style>
